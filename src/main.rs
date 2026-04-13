2b869a1a1597e888fc61256f898cd3bbf9e6c11b
guiyuanju <guiyuanjune@outlook.com>
2026-04-13 16:51:38 +0800

    add wallet

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+mod wallet;
+use crate::wallet::Wallet;
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Generate a new wallet file
+    NewWallet,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::NewWallet => Wallet::new().save()?,
+    }
+
+    Ok(())
 }
 
 #[cfg(test)]
